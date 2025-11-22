a19c0ced9398be7502d58cbb0a7ad83fd6cc5bb2
Matteo <mat.feliziani@gmail.com>
2025-11-22 16:39:33 +0100

    add cors policies

--- backend/main.py
+++ backend/main.py
@@ -8,8 +8,25 @@ from models import user as user_model
 from models import schemas
 import auth
 
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
